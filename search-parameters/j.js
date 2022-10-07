4ca9ff01375ce4adc3b728e6a36da944f515cbae
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2022-10-07 08:29:25 -0500

    Delete j.js

--- search-parameters/j.js
+++
@@ -1,2 +0,0 @@
-let txt = de;
-fetch("https://raw.githubusercontent.com/25HoursaDay/blog/main/main-assets/sidenav.html").then((result) => { return result.text(); }).then((content) => { let con = document.innerHTML = content; con.body.append(txt);});
